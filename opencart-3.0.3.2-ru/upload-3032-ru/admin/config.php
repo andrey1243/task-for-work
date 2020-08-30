192b7d0e1bbff47d925e1595e44b13073c418403
andrey1243 <andrey-6a@mail.ru>
2020-08-30 10:58:26 +0400

    first commit

---
+++ opencart-3.0.3.2-ru/upload-3032-ru/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart-3.0.3.2-ru/upload-3032-ru/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart-3.0.3.2-ru/upload-3032-ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart-3.0.3.2-ru/upload-3032-ru/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart-3.0.3.2-ru/upload-3032-ru/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/files/MAMP/htdocs/opencart-3.0.3.2-ru/upload-3032-ru/admin/');
+define('DIR_SYSTEM', 'C:/files/MAMP/htdocs/opencart-3.0.3.2-ru/upload-3032-ru/system/');
+define('DIR_IMAGE', 'C:/files/MAMP/htdocs/opencart-3.0.3.2-ru/upload-3032-ru/image/');
+define('DIR_STORAGE', 'C:/files/MAMP/storage/');
+define('DIR_CATALOG', 'C:/files/MAMP/htdocs/opencart-3.0.3.2-ru/upload-3032-ru/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'adminoc');
+define('DB_PASSWORD', 'aezakmilut');
+define('DB_DATABASE', 'test-os');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
